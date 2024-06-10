3007c2d9a575527d37bab22b1924bcf129b32523
lpc93 <168399822+lpc93@users.noreply.github.com>
2024-06-10 09:27:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -112,3 +112,9 @@ import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+        
+        
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
